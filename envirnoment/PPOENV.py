99f9ec5716223a8df0b514fe5f92effb0816afce
srikanth-444 <srikanth.popuri4444@gmail.com>
2025-08-31 16:36:35 -0600

    reward function with integrator alpha0.01---->0.1

--- envirnoment/PPOENV.py
+++ envirnoment/PPOENV.py
@@ -137,7 +137,7 @@ class PPOEnv:
         #     reward = - (error - 0.05) / (2 - 0.5)
         # else:
         #     reward = -10.0
-        alpha=0.01
+        alpha=0.1
         self.integral_error =(1-alpha)*self.integral_error+alpha*(current_lataccel - target) 
         reward = -((current_lataccel - target)**2 * 500 + jerk**2 * 1)-self.integral_error**2 
         # Done condition
